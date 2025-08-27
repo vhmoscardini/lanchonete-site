7dcad1e0c34b2bc356e90d0f3c6f4ba84ecc03e1
Victor Hugo <vhmoscardini.unifran@gmail.com>
2025-08-26 21:31:25 -0300

    arrumar pngs na sessao de cardapio

--- src/cardapio/cardapio.js
+++ src/cardapio/cardapio.js
@@ -26,14 +26,14 @@ function createCard(image, title, content) {
 	return card;
 }
 
-const card1 = createCard("https://cdn.discordapp.com/attachments/731329565544415324/914277255285444649/IlustracCCA7oCC83es-2D-e-3D-Grupo-Luz-1-877x1030-872x1024.png?ex=68aa3c8c&is=68a8eb0c&hm=89293f6b36505b5c3198c6acba4867778c5e6427f3bdacf4ec345dda51b4f342&","oi", "tudo bem")
-const card2 = createCard("https://media.discordapp.net/attachments/731329565544415324/1408110677217906898/Gy22GKCXAAAFOBZ.png?ex=68a9de42&is=68a88cc2&hm=c715fde1582501ee3a8b63f4e1ccd26eb773f092f1be8a2aadff3e5494769e98&=&format=webp&quality=lossless","olha que legall", "da pra criar varios")
-const card3 = createCard("https://pbs.twimg.com/media/GmlzMm2WwAAdLzI.jpg","sou um macaquinho", "gosto de comer banana")
-const card4 = createCard("https://pbs.twimg.com/media/GmlzMm2WwAAdLzI.jpg","sou um macaquinho", "gosto de comer banana")
-const card5 = createCard("https://pbs.twimg.com/media/GmlzMm2WwAAdLzI.jpg","sou um macaquinho", "gosto de comer banana")
-const card6 = createCard("https://pbs.twimg.com/media/GmlzMm2WwAAdLzI.jpg","sou um macaquinho", "gosto de comer banana")
-const card7 = createCard("https://pbs.twimg.com/media/GmlzMm2WwAAdLzI.jpg","sou um macaquinho", "gosto de comer banana")
-const card8 = createCard("https://pbs.twimg.com/media/GmlzMm2WwAAdLzI.jpg","sou um macaquinho", "gosto de comer banana")
+const card1 = createCard("/assets/a.png","oi", "tudo bem")
+const card2 = createCard("/assets/b.png","olha que legall", "da pra criar varios")
+const card3 = createCard("/assets/c.jpg","sou um macaquinho", "gosto de comer banana")
+const card4 = createCard("/assets/c.jpg","sou um macaquinho", "gosto de comer banana")
+const card5 = createCard("/assets/c.jpg","sou um macaquinho", "gosto de comer banana")
+const card6 = createCard("/assets/c.jpg","sou um macaquinho", "gosto de comer banana")
+const card7 = createCard("/assets/c.jpg","sou um macaquinho", "gosto de comer banana")
+const card8 = createCard("/assets/c.jpg","sou um macaquinho", "gosto de comer banana")
 
 cardContainer.appendChild(card1);
 cardContainer.appendChild(card2);
