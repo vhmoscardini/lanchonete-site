28f49adb94e6e34199790741981007fe173f920a
Victor Hugo <vhmoscardini.unifran@gmail.com>
2025-09-04 00:45:48 -0300

    adicionado funçao de pedido via whatsapp e outros ajustes

--- src/cardapio.js
+++ src/cardapio.js
@@ -1,8 +1,14 @@
 const cardContainer = document.getElementById("card-container");
 
-function createCard(image, title, content) {
+const cardCategory = document.createElement("div");
+cardCategory.classList.add("card-category");
+
+function createCard(image, title, price) {
 	const card = document.createElement("div");
 	card.classList.add("card");
+
+	const cardContent = document.createElement("div");
+	cardContent.classList.add("card-content");
 
 	const cardImage = document.createElement("img");
 	cardImage.setAttribute("src", image);
@@ -10,31 +16,48 @@ function createCard(image, title, content) {
 	const cardTitle = document.createElement("h2");
 	cardTitle.textContent = title;
 
-	const cardContent = document.createElement("p");
-	cardContent.textContent = content;
+	const cardPrice = document.createElement("p");
+	cardPrice.textContent = price;
 	
-	const cardButton = document.createElement("a");
+	//const cardButton = document.createElement("a");
+	const cardButton = document.createElement("button");
 	cardButton.textContent = "Pedir"
-	cardButton.setAttribute("href", "#");
 	cardButton.className = "card-button"
-
-	card.appendChild(cardImage);
-	card.appendChild(cardTitle);
+	cardButton.addEventListener("click", function() {
+		sendMessage(title);
+	}, false);
+	//cardButton.textContent = "Pedir"
+	//cardButton.setAttribute("href", `https://wa.me/+5516989953333?text=gostaria de pedir um ${title}`);
 	card.appendChild(cardContent);
-	card.appendChild(cardButton);
+
+	cardContent.appendChild(cardImage);
+	cardContent.appendChild(cardTitle);
+	cardContent.appendChild(cardPrice);
+	cardContent.appendChild(cardButton);
 
 	return card;
 }
+
+function sendMessage(name) {
+	let number = "+5516989953333"
+	var url = `https://wa.me/${number}?text=Olá, eu gostaria de pedir um ${name}`
+
+	window.open(url, "_blank").focus();
+}
+const section = document.createElement("h2");
+section.textContent = "lanches";
+cardContainer.appendChild(section);
 
 fetch("./cards.json")
 	.then(res => res.json())
 	.then(data => {
 		for (i in data.cards) {
 			card = createCard(data.cards[i].image, data.cards[i].name, data.cards[i].price)
-			cardContainer.appendChild(card);
+			cardCategory.appendChild(card);
 		}
 	})
 
+cardContainer.appendChild(cardCategory);
 // const card1 = createCard("/assets/a.png","oi", "tudo bem")
 // const card2 = createCard("/assets/b.png","olha que legall", "da pra criar varios")
 // const card3 = createCard("/assets/c.jpg","sou um macaquinho", "gosto de comer banana")
