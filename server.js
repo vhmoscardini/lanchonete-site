62b87cc82a3327f2ff1d180e11e4150d150f432b
Victor Hugo <vhmoscardini.unifran@gmail.com>
2025-10-24 18:16:05 -0300

    refazendo sistema de email

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const path = require("path");
-const nodemailer = require("nodemailer");
 
 const app = express();
 const port = 9000;
@@ -13,40 +12,40 @@ app.get("/", function(req, res) {
 	res.sendFile("src/index.html", {root: __dirname});
 });
 
-app.post("/send_email", function(req, res) {
-	var nome = req.body.nome;
-	var email = req.body.email;
-	var assunto = req.body.assunto;
-	var telefone = req.body.telefone;
-	var mensagem = req.body.mensagem;
-
-	var transporter = nodemailer.createTransport({
-		service: "gmail",
-		auth: {
-			user: "vhmoscardini.unifran@gmail.com",
-			pass: "ugpz niyr bsjr rlvi ",
-		}
-	})
-	
-	var mailOptions = {
-		from: email,
-		to: "vhmoscardini.unifran@gmail.com",
-		subject: "trollface",
-		html: `<h2>Nome:</h2>${nome}\n<h2>Email:</h2>${email}\n<h2>Telefone:</h2>${telefone}\n<h2>Mensagem:</h2>${mensagem}`,
-	}
-
-	transporter.sendMail(mailOptions, function(error, info) {
-		if (error) {
-			console.log(error)
-		}
-		else {
-			alert("conseguiu eba")
-		}
-		res.redirect("/");
-
-	})
-	console.log(nome, email, assunto, telefone, mensagem);
-});
+//app.post("/send_email", function(req, res) {
+//	var nome = req.body.nome;
+//	var email = req.body.email;
+//	var assunto = req.body.assunto;
+//	var telefone = req.body.telefone;
+//	var mensagem = req.body.mensagem;
+//
+//	var transporter = nodemailer.createTransport({
+//		service: "gmail",
+//		auth: {
+//			user: "vhmoscardini.unifran@gmail.com",
+//			pass: "ugpz niyr bsjr rlvi ",
+//		}
+//	})
+//	
+//	var mailOptions = {
+//		from: email,
+//		to: "vhmoscardini.unifran@gmail.com",
+//		subject: "trollface",
+//		html: `<h2>Nome:</h2>${nome}\n<h2>Email:</h2>${email}\n<h2>Telefone:</h2>${telefone}\n<h2>Mensagem:</h2>${mensagem}`,
+//	}
+//
+//	transporter.sendMail(mailOptions, function(error, info) {
+//		if (error) {
+//			console.log(error)
+//		}
+//		else {
+//			alert("conseguiu eba")
+//		}
+//		res.redirect("/");
+//
+//	})
+//	console.log(nome, email, assunto, telefone, mensagem);
+//});
 // app.get("/style.css", function(req, res) {
 // 	res.sendFile("./src/style.css", {root: __dirname});
 // });
---
+++ src/email.js
@@ -0,0 +1,29 @@
+const sendEmail = async (event) => {
+	event.preventDefault();
+	
+	let assuntoIndex = document.getElementById("assunto").selectedIndex;
+	let params = {
+		nome: document.getElementById("nome").value,
+		email: document.getElementById("email").value,
+		assunto: document.getElementById("assunto").options[assuntoIndex].text,
+		telefone: document.getElementById("telefone").value,
+		mensagem: document.getElementById("mensagem").value,
+	}
+	try {
+		const response = await emailjs.send(
+			"service_n02ujpt",
+			"template_sf8hvw3",
+			params,
+		);
+		alert("Email sent successfully!", response);
+	}
+	catch (error) {
+		alert("Failed to send email:", error);
+	}
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+	document
+		.getElementById("contato")
+		.addEventListener("submit", sendEmail);
+});
